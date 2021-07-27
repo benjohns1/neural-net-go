f7daa368394aac4678a0b7ce0f4ca661e8499c42
Ben Johns <benjohns1@gmail.com>
2021-07-27 00:42:24 -0500

    predict and train

--- main.go
+++ main.go
@@ -2,23 +2,33 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/exp/rand"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat/distuv"
+	"math"
 	"neural-net-go/matutil"
+	"time"
 )
 
 func main() {
-	fmt.Println("ok")
-	m := matutil.Scale(0.5, mat.NewDense(2, 2, []float64{0.5,1,2,3}))
-	fmt.Printf("%+v", m)
+	seed := uint64(time.Now().UnixNano())
+	nn := NewNetwork(Config{
+		input:  3,
+		hidden: 4,
+		output: 2,
+		rate:   0.01,
+		seed:   seed,
+	})
+	nn.Train([]float64{1,2,3}, []float64{0,1})
+	p := nn.Predict([]float64{1,2,3})
+	fmt.Printf("seed: %d, %+v", seed, p)
 }
 
 type Network struct {
-	inputs int
-	hiddens int
-	outputs int
+	cfg Config
+	src rand.Source
 	hiddenWeights *mat.Dense
 	outputWeights *mat.Dense
-	learningRate float64
 }
 
 type Config struct {
@@ -26,4 +36,73 @@ type Config struct {
 	hidden int
 	output int
 	rate float64
+	seed uint64
+}
+
+func NewNetwork(cfg Config) Network {
+	src := rand.NewSource(cfg.seed)
+	return Network{
+		cfg:           cfg,
+		src:           src,
+		hiddenWeights: mat.NewDense(cfg.hidden, cfg.input, randomArray(cfg.input*cfg.hidden, float64(cfg.input), src)),
+		outputWeights: mat.NewDense(cfg.output, cfg.hidden, randomArray(cfg.hidden*cfg.output, float64(cfg.hidden), src)),
+	}
+}
+
+func (n Network) Predict(inputData []float64) *mat.Dense {
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := matutil.Dot(n.hiddenWeights, inputs)
+	hiddenOutputs := matutil.Apply(sigmoid, hiddenInputs)
+	finalInputs := matutil.Dot(n.outputWeights, hiddenOutputs)
+	return matutil.Apply(sigmoid, finalInputs)
+}
+
+func (n *Network) Train(inputData []float64, targetData []float64) {
+	inputs := mat.NewDense(len(inputData), 1, inputData)
+	hiddenInputs := matutil.Dot(n.hiddenWeights, inputs)
+	hiddenOutputs := matutil.Apply(sigmoid, hiddenInputs)
+	finalInputs := matutil.Dot(n.outputWeights, hiddenOutputs)
+	finalOutputs := matutil.Apply(sigmoid, finalInputs)
+
+	targets := mat.NewDense(len(targetData), 1, targetData)
+	outputErrors := matutil.Subtract(targets, finalOutputs)
+	hiddenErrors := matutil.Dot(n.outputWeights.T(), outputErrors)
+
+	// backpropagate
+	n.outputWeights = matutil.Add(n.outputWeights,
+		matutil.Scale(n.cfg.rate,
+			matutil.Dot(matutil.Multiply(outputErrors, sigmoidPrime(finalOutputs)),
+				hiddenOutputs.T())))
+
+	n.hiddenWeights = matutil.Add(n.hiddenWeights,
+		matutil.Scale(n.cfg.rate,
+			matutil.Dot(matutil.Multiply(hiddenErrors, sigmoidPrime(hiddenOutputs)),
+				inputs.T())))
+}
+
+func sigmoid(_, _ int, z float64) float64 {
+	return 1.0 / (1 + math.Exp(-1*z))
+}
+func sigmoidPrime(m mat.Matrix) *mat.Dense {
+	rows, _ := m.Dims()
+	o := make([]float64, rows)
+	for i := range o {
+		o[i] = 1
+	}
+	ones := mat.NewDense(rows, 1, o)
+	return matutil.Multiply(m, matutil.Subtract(ones, m)) // m * (1 - m)
+}
+
+func randomArray(size int, v float64, src rand.Source) []float64 {
+	sqrtV := math.Sqrt(v)
+	dist := distuv.Uniform{
+		Min: -1 / sqrtV,
+		Max: 1 / sqrtV,
+		Src: src,
+	}
+	data := make([]float64, size)
+	for i := size-1; i >= 0; i-- {
+		data[i] = dist.Rand()
+	}
+	return data
 }
--- matutil/util.go
+++ matutil/util.go
@@ -3,49 +3,49 @@ package matutil
 import "gonum.org/v1/gonum/mat"
 
 // Dot product of 2 matrices.
-func Dot(m, n mat.Matrix) mat.Dense {
+func Dot(m, n mat.Matrix) *mat.Dense {
 	var o mat.Dense
 	o.Product(m, n)
-	return o
+	return &o
 }
 
 // Apply a function to all elements of a matrix.
-func Apply(fn func(i, j int, v float64) float64, m mat.Matrix) mat.Dense {
+func Apply(fn func(i, j int, v float64) float64, m mat.Matrix) *mat.Dense {
 	var o mat.Dense
 	o.Apply(fn, m)
-	return o
+	return &o
 }
 
 // Scale each element of a matrix.
-func Scale(s float64, m mat.Matrix) mat.Dense {
+func Scale(s float64, m mat.Matrix) *mat.Dense {
 	var o mat.Dense
 	o.Scale(s, m)
-	return o
+	return &o
 }
 
 // Multiply each corresponding element of the matrices together.
-func Multiply(m, n mat.Matrix) mat.Dense {
+func Multiply(m, n mat.Matrix) *mat.Dense {
 	var o mat.Dense
 	o.MulElem(m, n)
-	return o
+	return &o
 }
 
 // Add each corresponding element of the matrices together.
-func Add(m, n mat.Matrix) mat.Dense {
+func Add(m, n mat.Matrix) *mat.Dense {
 	var o mat.Dense
 	o.Add(m, n)
-	return o
+	return &o
 }
 
 // Subtract the corresponding elements of the second matrix from the first.
-func Subtract(m, n mat.Matrix) mat.Dense {
+func Subtract(m, n mat.Matrix) *mat.Dense {
 	var o mat.Dense
 	o.Sub(m, n)
-	return o
+	return &o
 }
 
 // AddScalar adds the value to each element in the matrix.
-func AddScalar(i float64, m mat.Matrix) mat.Dense {
+func AddScalar(i float64, m mat.Matrix) *mat.Dense {
 	r, c := m.Dims()
 	a := make([]float64, r*c)
 	for x := 0; x < r*c; x++ {
